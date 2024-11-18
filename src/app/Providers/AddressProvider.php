ec352bd277acde7a838711af17591eacdcca3c13
Santosh kumar <lucky1999mfp@gmail.com>
2024-11-18 09:15:03 +0530

    change adderss provider

--- src/app/Providers/AddressProvider.php
+++ src/app/Providers/AddressProvider.php
@@ -8,6 +8,6 @@ class AddressProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this-> loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+        $this-> publishesMigrations([__DIR__ . '/../../database/migrations'=> database_path('migrations')]);
     }
 }
